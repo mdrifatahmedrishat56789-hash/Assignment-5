6ab968b49ff7dff8a2d897119df834e2ca098b82
mdrifatahmedrishat56789-hash <mdrifatahmedrishat56789@gmail.com>
2026-04-24 18:08:00 +0600

    Login Page

---
+++ login.js
@@ -0,0 +1,21 @@
+document.getElementById("login").addEventListener('click',function(){
+    // user Name 
+    const userName = document.getElementById("userName")
+    const showUserName = userName.value
+    console.log(showUserName)
+    
+    // Password
+
+    const password = document.getElementById("password")
+    const showPassword = password.value
+    console.log(showPassword)
+
+    if(showUserName == 'admin' && showPassword == 'admin123'){
+        alert("Login Success")
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("Incorrect Password and User Name")
+    }
+
+})
