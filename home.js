9734485803f3f7d455813e579357d65dc9d7b3d8
mdrifatahmedrishat56789-hash <mdrifatahmedrishat56789@gmail.com>
2026-04-26 00:57:02 +0600

    border and open closed pic add

---
+++ home.js
@@ -0,0 +1,46 @@
+const allIssue = ()=>{
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+    fetch(url)
+    .then(res=> res.json())
+    .then(data => {
+        displayIssue(data.data)
+    })
+}
+
+
+
+
+const displayIssue = (issueCards)=>{
+    const issueCard = document.getElementById("issueCard")
+    issueCard.innerHTML = ""
+     
+    issueCards.forEach(card=>{
+        
+             const displayCard = document.createElement("div")
+        displayCard.innerHTML = `<div class="bg-white p-4 h-full rounded-xl border-t-2 ${card.status === "open" 
+            ? "border-t-green-500" : "border-t-yellow-600"} space-y-3">
+            <div class="flex justify-between">
+               ${card.status === "open" 
+  ? `<img src="./assets/Open-Status.png" alt="">` 
+  : `<img src="./assets/Closed- Status .png" alt="">`
+}
+                <p class="bg-[#EF4444] text-red text-xs rounded-2xl p-1 text-white">${card.priority}</p>
+            </div>
+
+            <div class="space-y-2">
+                <h3 class="text-xl font-bold ">${card.title}</h3>
+                <p class="text-[#64748B]">${card.description}</p>
+                <div class="flex gap-2">
+                    <p class="bg-yellow-400  text-xs rounded-xl p-1 text-red-700">Bug</p>
+                    <p class="bg-yellow-400  text-xs rounded-xl p-1 text-red-700">Help wanted</p>
+                </div>
+                <hr>
+                <p>#1 ${card.assignee}</p>
+                <p>${card.createdAt}</p>
+            </div>
+        </div>`
+
+        issueCard.appendChild(displayCard)
+        })
+}
+allIssue()
