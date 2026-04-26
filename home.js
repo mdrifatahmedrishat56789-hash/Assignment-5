93779ba40868ad10429fbfebaaead7ff68984152
mdrifatahmedrishat56789-hash <mdrifatahmedrishat56789@gmail.com>
2026-04-27 00:26:41 +0600

    loading add

--- home.js
+++ home.js
@@ -1,12 +1,20 @@
+const loading = document.getElementById("loading")
+
 const allIssue = ()=>{
+    loading.style.display = "flex";
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
     fetch(url)
     .then(res=> res.json())
     .then(data => {
         displayIssue(data.data)
+        loading.style.display = "none";
     })
 }
 
+const createElement = (arr) =>{
+    const htmlElement = arr.map((el)=> `<span class="bg-yellow-400  text-xs rounded-xl p-1 text-red-700">${el}</span>`)
+    return htmlElement.join("  ");
+}
 
 
 
@@ -28,11 +36,11 @@ const displayIssue = (issueCards)=>{
             </div>
 
             <div class="space-y-2">
-                <h3 class="text-xl font-bold ">${card.title}</h3>
+                <h3 class="text-xs md:text-xl font-bold ">${card.title}</h3>
                 <p class="text-[#64748B]">${card.description}</p>
                 <div class="flex gap-2">
-                    <p class="bg-yellow-400  text-xs rounded-xl p-1 text-red-700">Bug</p>
-                    <p class="bg-yellow-400  text-xs rounded-xl p-1 text-red-700">Help wanted</p>
+                    <p>${createElement(card.labels)}</p>
+                    
                 </div>
                 <hr>
                 <p>#1 ${card.assignee}</p>
@@ -41,6 +49,9 @@ const displayIssue = (issueCards)=>{
         </div>`
 
         issueCard.appendChild(displayCard)
-        })
+        })       
 }
+
+
+
 allIssue()
